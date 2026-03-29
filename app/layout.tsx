b7451bb36a01444a19b3c03a60e5a4eb84a58a78
Emanuel <emanuel@pop-os.local>
2026-03-29 15:47:26 -0300

    fix api

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="pt-br"
+      lang="PT-br"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
